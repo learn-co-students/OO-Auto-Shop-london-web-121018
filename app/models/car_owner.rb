0749054fc7fd7e2994a317c2daecf36a41e8f763
github username <github email address>
2018-12-17 11:20:22 +0000

    learn submit

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,34 @@
 class CarOwner
 
-  attr_reader :name
+  attr_reader :name, :mechanic
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+
+
+  def cars
+    Car.all.select do |car|
+      car.owner == self
+    end
+  end
+
+  def mechanics
+    cars.map {|car| car.mechanic}.uniq
+  end
+
+  def self.avg_cars
+     Car.all.length.to_f/CarOwner.all.length
+  end
+
+
 
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,31 @@
 class Mechanic
 
   attr_reader :name, :specialty
+  @@all = []
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select do |car|
+      self.specialty == car.classification
+    end
+  end
+
+  def owners
+    cars.map {|car| car.owner}
+  end
+
+  def owner_names
+    owners.map {|owner| owner.name}
+  end
 
 
 end
