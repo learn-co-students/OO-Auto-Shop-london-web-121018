b747fa345d396fda6b7addc211669614ad5d9e44
georgejeffery <georgejeffery@gmail.com>
2018-12-17 10:28:05 +0000

    Done.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,41 @@
 class Mechanic
 
-  attr_reader :name, :specialty
+  attr_reader :name, :specialty, :allmechanics
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@allmechanics = []
 
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@allmechanics << self
+  end
 
-end
+  def self.all
+    @@allmechanics
+  end
+
+  def services
+    carlist = []
+    allcars = Car.all
+    allcars.each do |car|
+      if car.classification == self.specialty
+        carlist << car
+      end
+    end
+    carlist
+  end
+
+  def carowners
+    carowners = []
+    owners = CarOwner.all
+    mechanics = owners.each do |owner|
+      owner.mechanics.each do |mechanic|
+        if mechanic == self
+          carowners << owner
+        end
+      end
+    end
+    carowners
+  end
+
+  end
