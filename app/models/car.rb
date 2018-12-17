589044aa9ae7d04b2427a43a8c1954f0b14b179a
seenevz <serneves@hotmail.com>
2018-12-17 16:42:15 +0000

    done

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,29 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :classification, :owner
 
-  def initialize(make, model, classification)
+  @@all = []
+
+  def initialize(make, model, classification, owner)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.car_classifications
+    car_class = []
+    self.all.each {|car| car_class << car.classification}
+    car_class.uniq
+  end
+
+  def qualified_mechanic
+    Mechanic.all.select {|mechanic| mechanic.specialty == self.classification}
   end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,29 @@ class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
+
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars_served
+    Car.all.select {|car| car.classification == self.specialty}
+  end
+
+  def owners_by_mechanic
+    cars_served.map {|car| car.owner}
+  end
+
+  def owners_name_by_mechanic
+    owners_by_mechanic.map {|owner| owner.name}.uniq
+  end
 
 
 end
