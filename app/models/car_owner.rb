24f5a0d45cc446d58c0eb64f979570b21c708709
github username <github email address>
2018-12-17 22:49:51 +0000

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,37 @@
 class CarOwner
-
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    self.class.all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def cars
+    Car.all.select {|car| car.owner == self}
+  end
+
+  def car_classifications
+    cars.map {|car| car.classification}.uniq
+  end
+
+  def mechanics
+    suitable_mechanics = []
+    car_classifications.each do |classification|
+      Mechanic.all.each do |mechanic|
+        if classification == mechanic.specialty
+          suitable_mechanics << mechanic.name
+        end
+      end
+    end
+    suitable_mechanics
+  end
+
+  def self.average_num_of_cars
+    Car.all.length.to_f/self.all.length
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,26 @@
 require_relative '../config/environment.rb'
 
+
 def reload
   load 'config/environment.rb'
 end
 
+# Test Data
+
+o1 = CarOwner.new("Harry")
+o2 = CarOwner.new("Jack")
+o3 = CarOwner.new("Sam")
+
+m1 = Mechanic.new("Tom", "antique")
+m2 = Mechanic.new("Mark", "beamer")
+m3 = Mechanic.new("Keith", "exotic")
+m4 = Mechanic.new("Steve", "beamer")
+
+c1 = Car.new(o1, "Aston Martin", "DB4", "antique")
+c2 = Car.new(o1, "Toyata", "S89", "beamer")
+c3 = Car.new(o3, "Mustang", "High-top", "beamer")
+c4 = Car.new(o1, "Ferrari", "Vantage", "exotic")
+c5 = Car.new(o2, "Mercedes", "Classic", "antique")
+
 binding.pry
+p 'eof'
